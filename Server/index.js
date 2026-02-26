eb9d1ff426b4921917b00a683b53d08778658153
KEYUR141 <bravo141.kyr@gmail.com>
2026-02-26 16:04:34 +0530

    URL implemetation and node install for url

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,21 @@
 const http = require('http');
 const fs = require('fs')
-
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    
+    if (req.url ==='/favicon.ico') return res.end();
     const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`
+    my_url = url.parse(req.url,true)
+    console.log(my_url)
     fs.appendFile('server.log', log, (err)=> {
         if (err) {
             console.log(err)
         }
     })  
-    console.log(req.headers)
     
-    res.end("Hello, World!")
+    // console.log(req.headers)
+    name = my_url.query.name
+    res.end(`Hello World!,${name}`)
 })
 
 
